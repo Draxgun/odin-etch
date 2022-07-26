0b243db8a595a5ca573d6c0e88deb49232347ec5
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-26 12:55:23 -0600

    Grid Coloring option done

--- javascript.js
+++ javascript.js
@@ -1,24 +1,34 @@
 
-let n = 16;
+let n = 100;
 
 let mainContainer = document.querySelector(".mainContainer");
 
 let mainGrid = document.querySelector(".mainGrid");
 mainGrid.style.gridTemplateColumns = `repeat(${n},1fr)`
+
 /*Function that creates grid */
 let createGrid = (n) => {
     for(let i=1; i<=n*n;i++) {        
         let grid = document.createElement('div');
         grid.classList.add("grid");
         grid.setAttribute('id', `grid${i}` );
-        /*grid.textContent = i; */
-        
         mainGrid.appendChild(grid);
     }
 }
 
-
 createGrid(n);
+
+/* Makes me get all elements */    
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach(grid => {
+  grid.addEventListener('click', function() {
+    grid.setAttribute('style', 'background-color: yellow;');
+  });
+});
+
+
+
 
 
 
