3cbd08d77d6d637d52ec5ee94fd054b5d3a93186
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-26 12:24:11 -0600

    Responsive grid done

--- javascript.js
+++ javascript.js
@@ -1,17 +1,24 @@
-console.log("Hello world")
 
+let n = 100;
 
 let mainContainer = document.querySelector(".mainContainer");
-let mainGrid = document.querySelector(".mainGrid")
 
+let mainGrid = document.querySelector(".mainGrid");
+mainGrid.style.gridTemplateColumns = `repeat(${n},1fr)`
 /*Function that creates grid */
 let createGrid = (n) => {
-    for(let i=1; i<n*n;i++) {
+    for(let i=1; i<=n*n;i++) {        
         let grid = document.createElement('div');
         grid.classList.add("grid");
         grid.setAttribute('id', `grid${i}` );
+        /*grid.textContent = i; */
+        
         mainGrid.appendChild(grid);
     }
 }
 
-createGrid(10);
+
+createGrid(n);
+
+
+
